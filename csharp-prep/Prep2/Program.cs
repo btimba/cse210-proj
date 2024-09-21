240e1806114113e4efc9dba4c8fbd4f9ddc961fe
Brian Timba <tim24003@byui.edu>
2024-09-21 22:59:32 +0200

    Done prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+      
+        
+        Console.Write("What is your grade percentage?");
+        string percentage = Console.ReadLine();
+        int grade = int.Parse(percentage);
+
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.Write($"Your grade is {letter}, ");
+
+        if (grade >= 70)
+        {
+            Console.Write("Congratulations you have passed!");
+        }
+        else 
+        {
+            Console.Write("Sorry, better luck next time");
+        }
+
     }
 }
